7b7eba4c789943cdb2baf059148bd78f6c052ad7
shivani <shivanimendhule6@gmail.com>
2024-06-19 21:59:14 +0530

    added some changes in app.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>shivani the DEVI </h1>
+      <h1>Github new project and i am not a devi ok</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
