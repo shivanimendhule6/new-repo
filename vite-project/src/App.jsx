28e247d9989bd3471e674bb0d5f814a9e03ef619
shivani <shivanimendhule6@gmail.com>
2024-06-23 22:00:01 +0530

    Added reset button and alert

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,8 +5,23 @@ import './App.css'
 
 export default function Counter() {
   const [count, setCount] = useState(0);
-  const handelIncrement = () => setCount(count + 1);
-  const handelDecrement = () => setCount(count - 1);
+  const handelIncrement = () => {
+    if (count < 10) {
+      setCount(count + 1);
+    }
+    else if (count === 10) {
+      alert("Count has reached 10!");
+    } 
+  }
+  const handelDecrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+    else if (count === 0) {
+      alert("Count has reached 0!");
+    } 
+  }
+  const resetDefault = () => setCount(0);
 
   return (
     <div className="d-flex jutify-content-center align-items-center">
@@ -20,6 +35,11 @@ export default function Counter() {
           </button>
           <button onClick={handelDecrement} className="btn btn-danger ms-3">
             Decrement
+          </button>
+        </div>
+        <div className="d-flex justify-content-center mt-3">
+          <button onClick={resetDefault} className="btn btn-info">
+            Reset
           </button>
         </div>
       </div>
